177ff5dd0a07938381e86677553f83f44d7a7131
Szymon <ilikedzimi@gmail.com>
2018-12-23 21:02:25 +0100

    AboutFragment class

---
+++ SamplerApp/AboutFragment.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+
+namespace SamplerApp
+{
+    public class AboutFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.about_fragment, container, false);
+            return view;
+               
+        }
+    }
+}
