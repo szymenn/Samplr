851fca0d1f098b2e61d30b2d7be11c9b1723dec8
Szymon <ilikedzimi@gmail.com>
2018-12-23 21:01:42 +0100

    WelcomeFragment class

---
+++ SamplerApp/WelcomeFragment.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+
+namespace SamplerApp
+{
+    public class WelcomeFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.welcome_fragment, container, false);
+            return view;
+        }
+    }
+}
